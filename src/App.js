2fb289b32f0756673d6f86c1a132986f49d3977a
bluo47 <benjaminpluo2001@gmail.com>
2024-12-31 14:08:27 -0500

    test commit to deploy

--- src/App.js
+++ src/App.js
@@ -8,18 +8,18 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello from Render!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Testing commit to deploy. Deploying at 2:10PM 12/31/2024.</p>
         </header>
+        <footer>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+          </a>
+        </footer>
       </div>
     );
   }
